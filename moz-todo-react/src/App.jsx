ba3fb5fc17501a9fa92b1c5bbff68c1de3724ea0
Fuky9 <lukasfukan6@gmail.com>
2024-05-06 22:50:41 +0200

    Added interactivity to Todo app(adding tasks, synchronizing toggle complete with browser and added delete functionality

--- moz-todo-react/src/App.jsx
+++ moz-todo-react/src/App.jsx
@@ -1,28 +1,60 @@
 import Form from "./components/Form";
 import FilterButton from "./components/FilterButton";
 import Todo from "./components/Todo";
+import { useState } from "react";
+import { nanoid } from "nanoid";
 
 function App(props) {
+    const[tasks, setTasks] = useState(props.tasks);
+
+    // Function for adding task to our Todo list
+    const addTask = (name) => {
+        const newTask = {id:`todo-${nanoid()}`, name, completed: false};
+        setTasks([...tasks, newTask]);
+    }
+
+    // Function to change completed property of checkboxes, based on checked/not checked
+    const toggleTaskCompleted = (id) => {
+        const updatedTasks = tasks.map((task) => {
+            if (id === task.id) {
+                return {...task, completed: !task.completed}
+            } return task;
+        });
+        setTasks(updatedTasks);
+    }
+
+    // Function for deleting tasks
+    const deleteTask = (id) => {
+        const remainingTasks = tasks.filter((task) => id !== task.id);
+        setTasks(remainingTasks);
+    }
+
     // You should always pass a unique key to anything you render with iteration
-    const taskList = props.tasks?.map((task) => (
+    const taskList = tasks?.map((task) => (
     <Todo
     id={task.id}
     name={task.name}
     completed={task.completed}
     key={task.id}
+    toggleTaskCompleted={toggleTaskCompleted}
+    deleteTask={deleteTask}
     />
     ));
+
+    // Taking care of right number of tasks
+    const taskNoun = taskList.length !==1 ? "tasks" : "task";
+    const headingText = `${taskList.length} ${taskNoun} remaining`;
 
     return(
         <div className="todoapp stack-large">
             <h1>TodoMatic</h1>
-            <Form />
+            <Form addTask={addTask} />
             <div className="filters btn-group stack-exception">
                     <FilterButton />
                     <FilterButton />
                     <FilterButton />
             </div>
-            <h2 id="list-heading">3 tasks remaining</h2>
+            <h2 id="list-heading">{headingText}</h2>
             <ul role="list" className="todo-list stack-large stack-exception" aria-labelledby="list-heading">
                 {/* <Todo name="Eat" id="todo-0" completed />
                 <Todo name="Sleep" id="todo-1"/>
--- moz-todo-react/src/components/Form.jsx
+++ moz-todo-react/src/components/Form.jsx
@@ -1,13 +1,27 @@
-const Form = () => {
+import { useState } from "react";
+
+const Form = (props) => {
+    const [name, setName] = useState("");
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        // Using Callback props(callback function) to send data from child to parent
+        props.addTask(name);
+        setName("");
+        }
+
+    const handleChange = (event) => {
+        setName(event.target.value);
+    }
     return(
-            <form>
+            <form onSubmit={handleSubmit}>
                 <h2 className="label-wrapper">
                     <label htmlFor="new-todo-input" className="label__lg">
                         What needs to be done?
                     </label>
                 </h2>
                 <input type="text" id="new-todo-input" className="input input__lg"
-                name="text" autoComplete="off" />
+                name="text" autoComplete="off" value={name} onChange={handleChange} />
                 <button type="submit" className="btn btn__primary btn__lg">
                     Add
                 </button>
